ec2be909e430d536c256611145f89bb5c3777712
Erik Istre <e.istre91@gmail.com>
2016-02-11 12:54:48 +1300

    Add fibs2, a more efficient fibonacci number computer

--- CS194/Week6/Fibonacci.hs
+++ CS194/Week6/Fibonacci.hs
@@ -9,9 +9,16 @@ fibs1 :: [Integer]
 fibs1 = map fib [0..]
 
 addTwo :: [Integer] -> [Integer]
-addTwo [] 		= []
-addTwo [x] 		= [x]
-addTwo (x:y:xs) = (x+y):x:y:xs
-
+addTwo t = let 
+			rt = reverse t
+		   in
+			case rt of
+				[]			-> []
+				[x]			-> [x]
+				(x:y:xs)	-> reverse ((x+y):x:y:xs)
+				
 fibs2 :: [Integer]
-fibs2 = foldr _ [] [0..]
+fibs2 = foldr (\x acc -> addTwo acc) [0, 1] [0..]
+
+fibs2finite :: Integer -> [Integer]
+fibs2finite n = foldr (\x acc -> addTwo acc) [0, 1] [0..n]
