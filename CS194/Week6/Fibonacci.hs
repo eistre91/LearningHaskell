398d7352f784fbc59910f3845c1b8294a720b310
Erik Istre <e.istre91@gmail.com>
2016-02-11 13:21:14 +1300

    Add Stream data type constructor, streamToList function and Show for Streams

--- CS194/Week6/Fibonacci.hs
+++ CS194/Week6/Fibonacci.hs
@@ -22,3 +22,29 @@ fibs2 = foldr (\x acc -> addTwo acc) [0, 1] [0..]
 
 fibs2finite :: Integer -> [Integer]
 fibs2finite n = foldr (\x acc -> addTwo acc) [0, 1] [0..n]
+
+data List t = E | C2 t (List t)
+	deriving (Show)
+
+lst1 :: List Int
+lst1 = C2 3 (C2 5 (C2 2 E))
+
+data Stream a = C a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (C x y) = x:(streamToList y)
+
+instance Show a => Show (Stream a) where
+	show x = show (take 20 (streamToList x))
+
+streamRepeat :: a -> Stream a	
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+	
+--st1 :: Stream Int
+--st1 = C 0 (Stream 0)
+--Note this doesn't actually work.
+--Stream itself isn't a constructor, but C is.
+--Can't type out the construction of a Stream.
